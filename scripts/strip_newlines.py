7908886ad9fc0346f86c7b703babad33b1af8e44
deprekate <deprekate@gmail.com>
2020-03-02 15:45:40 -0800

    Switched to csvreader

--- scripts/strip_newlines.py
+++ scripts/strip_newlines.py
@@ -2,31 +2,36 @@
 
 import os
 import sys
+import zipfile
+import csv
+import codecs
 
 if len(sys.argv) < 2:
 	print("usage: strip_newlines.py INFILE")
 	exit()
 
 flag = False
+content = ""
 
-with open(sys.argv[1]) as file_in:
-	name_in = os.path.splitext(sys.argv[1])
-	name_out = name_in[0] + "_clean" + name_in[1]
-	with open(name_out, 'w') as file_out:
-		for line in file_in:
-			if line.rstrip().endswith("\"\"\""):
-				flag = True
-				file_out.write(line.rstrip())
-				file_out.write("\t")
-			elif line.startswith("\"\"\""):
-				flag = False
-				file_out.write(line.rstrip())
-				file_out.write("\n")
-			elif flag:
-				file_out.write(line.rstrip())
-				file_out.write("\t")
-			else:
-				file_out.write(line.rstrip())
-				file_out.write("\n")
+with zipfile.ZipFile(sys.argv[1]) as z:
+	name_in = z.namelist()[0]
+	with z.open(name_in) as file_in:
+		reader = csv.reader(codecs.iterdecode(file_in, 'utf-8'))
+		for line in reader:
+			print(line)
 
+
+'''
+		name_in = os.path.splitext(name_in)
+		name_out = name_in[0] + "_clean" + name_in[1]
+		with open(name_out, 'w') as file_out:
+			for line in file_in:
+				print(line)
+				if re.search(",[01],[01],[01],[01],[01],[01]", line):
+					file_out.write(content)
+					file_out.write(line)
+					content = ""
+				else:
+					content += line.rstrip()
+'''
 	
