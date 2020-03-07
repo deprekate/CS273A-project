65c221c8cc6333413dc10aed203813d24fa9b1e8
deprekate <deprekate@gmail.com>
2020-03-06 20:34:46 -0800

    Messed with some stuff

---
+++ R/randomForest.R
@@ -0,0 +1,31 @@
+library(randomForest)
+#library(caret)
+#library(ggbiplot)
+
+# module load R/3.6.2
+
+
+Xz = gzfile('matrix.csv.gz','rt')  
+Y = read.csv(Xz,header=F) 
+
+Yz = gzfile('classes.csv.gz','rt')  
+Y = read.csv(Yz,header=F) 
+
+# random Forest
+print('loaded data')
+dat.rf <- randomForest(
+			type='classification',
+			X, 
+			Y$V1, 
+			prox=FALSE,
+)
+dat.rf
+varImpPlot(dat.rf)
+
+ImpMeasure<-data.frame(importance(dat.rf))
+ImpMeasure$Vars<-row.names(ImpMeasure)
+#important_genus <- ImpMeasure[ImpMeasure$MeanDecreaseGini > 0,2]
+
+Yhat <- predict(dat.rf, X)
+
+confusionMatrix(Yhat, Y)
