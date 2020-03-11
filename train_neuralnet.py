2e152ab6571b8e65313119a81e045124b7c462d7
deprekate <deprekate@gmail.com>
2020-03-10 19:36:05 -0700

    Need to get correct loss function

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -17,11 +17,23 @@ stemmer = nltk.PorterStemmer()
 stop_words = stopwords.words('english')
 
 
-#import tensorflow as tf
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras import backend as K
+
+logits = tf.constant([[0, 1],
+                      [1, 1],
+                      [2, -4]], dtype=tf.float32)
+y_true = tf.constant([[1, 1],
+                      [1, 0],
+                      [1, 0]], dtype=tf.float32)
+# tensorflow api
+#loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=y_true, logits=logits)
+#tf.compat.v1.losses.sigmoid_cross_entropy(multi_class_labels=y_true, logits=logits)
+
 
 
 if len(sys.argv) != 2:
@@ -39,11 +51,11 @@ def remove_non_ascii(text):
 	return ''.join(i for i in text if ord(i)<128)
 
 def destem(word):
-        # the stemmer code breaks on really long words
-        if len(word) < 50:
-                return stemmer.stem(word)
-        else:
-                return word
+	# the stemmer code breaks on really long words
+	if len(word) < 50:
+		return stemmer.stem(word)
+	else:
+		return word
 
 def clean(text):
 	# CHARACTER LEVEL CLEANING
@@ -80,23 +92,36 @@ def clean_list(a):
 
 	return b
 
-def create_model(input_dim):
-    '''
-    This creates and returns a new model
-    '''
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model = keras.Sequential([
-        keras.layers.Dense(5000, activation='relu', input_shape=(input_dim,)),
-        keras.layers.Dropout(0.1),
-        keras.layers.Dense(1000, activation='relu',),
-        keras.layers.Dropout(0.1),
-        keras.layers.Dense(6, activation='sigmoid')
-    ])
-    model.compile(optimizer = sgd,
-                  metrics=['accuracy'],
-                  loss='binary_crossentropy'
-    )
-    return model
+def get_weighted_loss(my_input):
+	def weighted_bce(y_true, y_pred):
+		#cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(_pred) + (1-y)*tf.log(1-_pred), reduction_indices=1))
+		weights = (y_true * my_input) + 1.
+		bce = K.binary_crossentropy(y_true, y_pred)
+		weighted_bce = K.mean(bce * weights)
+		return weighted_bce
+
+def create_model(input_dim, my_loss):
+	'''
+	This creates and returns a new model
+	'''
+	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+	model = keras.Sequential([
+		keras.layers.Dense(5, activation='relu', input_shape=(input_dim,)),
+		keras.layers.Dropout(0.1),
+		keras.layers.Dense(10, activation='relu',),
+		keras.layers.Dropout(0.1),
+		keras.layers.Dense(6, activation='sigmoid')
+	])
+	model.compile(
+		optimizer = sgd,
+		#optimizer = 'Adam',
+		metrics=['accuracy'],
+		#loss='binary_crossentropy'
+		#loss=tf.compat.v1.losses.sigmoid_cross_entropy
+		loss=my_loss
+	)
+	return model
 
 class my_tokenizer(dict):
 	def __init__(self, num_words=None):
@@ -128,7 +153,7 @@ class my_tokenizer(dict):
 
 # ----------------------------TRAINING----------------------
 
-num_words = 10000
+num_words = 10
 t = Tokenizer(num_words, lower=True, oov_token=None)
 tt = my_tokenizer(num_words=num_words)
 
@@ -139,34 +164,37 @@ with zipfile.ZipFile(sys.argv[1]) as z:
 	with z.open(name_in) as file_in:
 		# read in data
 		tr = pd.read_csv(file_in)
-		#Xtr = tr['comment_text']
+		Xtr = tr['comment_text']
 		Ytr = tr.loc[:,'toxic':'identity_hate'].values
 		
 		# comment or uncomment these as needed for speed
 		#pickle.dump(clean_list(Xtr), open( "cleaned_comments.p", "wb" ) )
-		Xtr = pickle.load( open( "cleaned_comments.p", "rb" ) )
+		#Xtr = pickle.load( open( "cleaned_comments.p", "rb" ) )
 
 		# tokenize data
-		#t.fit_on_texts(list(Xtr))
-		#Xtr = t.texts_to_matrix(Xtr, mode='count')
+		t.fit_on_texts(list(Xtr))
+		Xtr = t.texts_to_matrix(Xtr, mode='count')
 
 		# these are my custom code to tokenize
-		tt.add_texts(Xtr)
-		Xtr = tt.texts_to_matrix(Xtr)
+		#tt.add_texts(Xtr)
+		#Xtr = tt.texts_to_matrix(Xtr)
 
 		# dont do these
 		#Xtr = t.texts_to_sequences(Xtr)
 		#Xtr = pad_sequences(Xtr, num_words)
 
 
+print(Xtr.shape)
+print(Ytr.shape)
 # THIS IS TO DUMP WORD INFO
 #print(t.word_counts)
 #print(t.document_count)
 #print(t.word_index)
 #print(t.word_docs)
 
+
 # this is to dump the matrix
-if 1:
+if 0:
 	for word in tt.words_used():
 			print(word, ',', sep='', end='')
 	print()
@@ -174,17 +202,20 @@ if 1:
 		for col in row:
 			print(int(col), ",", sep='', end='')
 		print()
-	
-exit()
 
-model = create_model(Xtr.shape[1])
-model.fit(Xtr, Ytr, epochs=3, batch_size=2000) #, callbacks=[cp_callback])
+
+#cost = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=y_*10)
+#cost = tf.compat.v1.losses.sigmoid_cross_entropy(multi_class_labels=Ytr, logits)
+cost = 'binary_crossentropy'
+
+model = create_model(Xtr.shape[1], cost)
+model.fit(Xtr, Ytr, epochs=3) #, batch_size=2000) #, callbacks=[cp_callback])
 #test_loss, test_acc = model.evaluate(Xtr,  Ytr, verbose=2)
 #print('\nTest accuracy of', 'Adam', 'Model:', test_acc)\
 
 Yhat = model.predict(Xtr)
-for row in Yhat:
-	print(np.round(row, 5))
+for x, y, yh in zip(Xtr, Ytr, Yhat):
+	print(x, y, np.round(yh, 5), sep='\t')
 
 
 # ----------------------------TESTING-----------------------
